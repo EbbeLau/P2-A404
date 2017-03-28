9c11f1768a950a0d1f5462e7f38e29dbaa6ba5ac
MrLongbottom <perich16@student.aau.dk>
2017-03-28 09:13:36 +0200

    added test

--- ArtificialNeuralNetwork/ArtificialNeuralNetwork/Program.cs
+++ ArtificialNeuralNetwork/ArtificialNeuralNetwork/Program.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace ArtificialNeuralNetwork
 {
+    //test
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
