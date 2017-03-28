3f45f36f56e99503ef32a58336a23f326227d11e
MrLongbottom <perich16@student.aau.dk>
2017-03-28 09:53:03 +0200

    Addid basic initilization

--- ArtificialNeuralNetwork/ArtificialNeuralNetwork/Program.cs
+++ ArtificialNeuralNetwork/ArtificialNeuralNetwork/Program.cs
@@ -8,15 +8,33 @@ namespace ArtificialNeuralNetwork
 {
     class NeuralNetwork
     {
-
+        //Variables
+        public Layer[] layers;
+        
+        //Constructor
+        public NeuralNetwork (int[] size)
+        {
+            layers = new Layer[size.Length];
+            for (int i = 0; i < layers.Length; i++)
+            {
+                layers[i] = new Layer(size[i]);
+            }
+        }
     }
 
     class Layer
     {
+        //Variables
+        public Neuron[] neurons;
 
+        //Constructor
+        public Layer (int size)
+        {
+            neurons = new Neuron[size];
+        }
     }
 
-    class Node
+    class Neuron
     {
 
     }
