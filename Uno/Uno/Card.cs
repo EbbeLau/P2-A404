ee3398adb00513c1ac43712fc48358537acc3efb
Rasmus Christensen <slayeren3024@gmail.com>
2017-03-30 14:04:19 +0200

    Card Functionality

--- Uno/Uno/Card.cs
+++ Uno/Uno/Card.cs
@@ -8,5 +8,26 @@ namespace Uno
 {
     class Card
     {
+        public enum CardColor
+        {
+            Red, Blue, Green, Yellow, Black
+        }
+
+        public enum CardValue
+        {
+            Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Block, PlusTwo, PlusFour, Recolor, Reverse
+        }
+
+        CardColor Color;
+        CardValue Value;
+
+        public Card(CardColor color , CardValue value)
+        {
+            Color = color;
+            Value = value;
+        }
+
+
+
     }
 }
